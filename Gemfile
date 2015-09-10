7123382f6646d588cfa76fafcd3b48b31da0432a
Jonas Nicklas <jonas.nicklas@gmail.com>
2015-09-10 13:31:38 +0200

    Add crud for posts

--- Gemfile
+++ Gemfile
@@ -1,12 +1,15 @@
 source 'https://rubygems.org'
 
 gem "refile", require: "refile/rails", github: "refile/refile"
+gem "refile-mini_magick"
+gem "refile-s3"
 
 gem "rails", "4.2.3"
 gem "sqlite3"
 gem "sass-rails", "~> 5.0"
 gem "uglifier", ">= 1.3.0"
 gem "coffee-rails", "~> 4.1.0"
+gem "twitter-bootstrap-rails"
 
 gem "jquery-rails"
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,46 @@ class PostsController < ApplicationController
   def index
     @post = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :posts
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :body)
+  end
 end
